001dec01d9dbbc11f6fc772d396fbd173f085742
ToritenKabosu <204140495+ToritenKabosu@users.noreply.github.com>
2026-07-19 09:22:18 +0900

    Initial commit

---
+++ src/Logger.cs
@@ -0,0 +1,47 @@
+﻿namespace Restarter;
+
+public static class Logger
+{
+    public static void Ask(string msg)
+    {
+        Console.ForegroundColor = ConsoleColor.Cyan;
+        Console.Write("[ASK]");
+        Console.ResetColor();
+        Console.Write(" " + msg);
+    }
+    public static void Info(string msg)
+    {
+        Console.ForegroundColor = ConsoleColor.Gray;
+        Console.Write("[INFO]");
+        Console.ResetColor();
+        Console.Write(" " + msg);
+    }
+    public static void Run(string msg)
+    {
+        Console.ForegroundColor = ConsoleColor.Yellow;
+        Console.Write("[RUN]");
+        Console.ResetColor();
+        Console.Write(" " + msg);
+    }
+    public static void RunInline(string msg)
+    {
+        Console.ForegroundColor = ConsoleColor.Yellow;
+        Console.Write("\r[RUN]");
+        Console.ResetColor();
+        Console.Write(" " + msg + "   ");
+    }
+    public static void Ok(string msg)
+    {
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.Write("[OK]");
+        Console.ResetColor();
+        Console.WriteLine("  " + msg);
+    }
+    public static void Err(string msg)
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.Write("[ERR]");
+        Console.ResetColor();
+        Console.WriteLine(" " + msg);
+    }
+}
---
+++ src/Program.cs
@@ -0,0 +1,57 @@
+﻿using System.Diagnostics;
+
+namespace Restarter;
+
+public static class Program
+{
+    public static void Main(string[] args)
+    {
+        if (args.Contains("--townofhost_fun"))
+        {
+            Logger.Run("Searching Among Us.exe...");
+            var directoryPath = AppContext.BaseDirectory;
+            DirectoryInfo dir = new DirectoryInfo(directoryPath);
+            string gameDir = dir.Parent?.Parent?.Parent?.FullName ?? "";
+            if (string.IsNullOrEmpty(gameDir))
+            {
+                Logger.Err("Among Us.exe not found.");
+                WaitAndExit();
+                return;
+            }
+            string amongUsExePath = Path.Combine(gameDir, "Among Us.exe");
+            if (!File.Exists(amongUsExePath))
+            {
+                Logger.Err("Among Us.exe not found.");
+                WaitAndExit();
+                return;
+            }
+            Logger.Ok("Done!");
+
+            Logger.Run("Waiting for old process to exit...");
+            Thread.Sleep(2000);
+            Logger.Ok("Done!");
+
+            Logger.Run("Among Us.exe starting...");
+            Process.Start(amongUsExePath);
+            Logger.Ok("Done!");
+
+        }
+        else
+        {
+            Logger.Info("このプログラムは手動で起動するものではありません。");
+            Console.WriteLine();
+            Logger.Info("This program should not be launched manually.");
+            WaitAndExit();
+        }
+
+    }
+
+    private static void WaitAndExit()
+    {
+        Console.WriteLine();
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.WriteLine("Press any key to exit...");
+        Console.ForegroundColor = ConsoleColor.White;
+        Console.ReadKey();
+    }
+}
